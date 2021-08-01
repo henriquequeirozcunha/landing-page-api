9756ac1edc5f983fe995e9db92ae8b5eb60f58a8
Henrique Cunha <henrique.cunha@corpus.inf.br>
2021-08-01 16:11:11 -0300

    feat: add netlify webhook

--- api/landing-page/models/landing-page.js
+++ api/landing-page/models/landing-page.js
@@ -1,8 +1,18 @@
-'use strict';
+"use strict";
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
- * to customize this model
- */
+const axios = require("axios");
+const netlifyWebHook = strapi.config.get("custom.netlifyWebHook");
 
-module.exports = {};
+module.exports = {
+  /**
+   * Triggered before user creation.
+   */
+  lifecycles: {
+    async afterCreate(_, __) {
+      netlifyWebHook && axios.post(netlifyWebHook);
+    },
+    async afterUpdate(_, __, ___) {
+      netlifyWebHook && axios.post(netlifyWebHook);
+    },
+  },
+};
